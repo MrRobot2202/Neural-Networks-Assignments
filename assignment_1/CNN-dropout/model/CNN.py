225119fa8ce9fc38289f83ea2bfe38b3275da85f
kathi_19 <e.pirjolea@student.rug.nl>
2025-09-28 17:26:52 +0200

    ex 3 CNN with dropout

--- assignment_1/CNN-dropout/model/CNN.py
+++ assignment_1/CNN-dropout/model/CNN.py
@@ -8,19 +8,17 @@ class CNN(nn.Module):
 
         # Conv layer 1: in_channels=3 (RGB), out_channels=32
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
-
         # Conv layer 2: 32 → 64
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-
         # Conv layer 3: 64 → 128
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-
         # Fully connected layer (after flattening)
         self.fc1 = nn.Linear(128 * 4 * 4, 10)
 
         # Pooling
         self.pool = nn.AvgPool2d(2, 2)
-
+        # Dropout (non-trainable)
+        self.dropout = nn.Dropout(0.5)
 
     def forward(self, x, get_features=False):
         features = []
@@ -39,11 +37,12 @@ class CNN(nn.Module):
         x = F.relu(self.conv3(x))
         x = self.pool(x)
         features.append(x)
+        x = self.dropout(x)
 
         # Flatten
         x = x.view(x.size(0), -1)
 
-        # FC
+        # Fully connected layer
         x = self.fc1(x)
 
         if get_features:
